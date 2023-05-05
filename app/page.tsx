e8e3452a526a48ca11b7df899d05dfcffd2c5f24
hannah68 <reihaneh.naderi1989@gmail.com>
2023-05-05 10:07:12 +0100

    add profile section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@
 
 export default function Home() {
   return (
-    <main> 
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Hannah</span>.
+        </span>
+      </p>
     </main>
   )
 }
