f6aeca990d70fd55c741ca132cbe772d2327a76e
hannah68 <reihaneh.naderi1989@gmail.com>
2023-05-09 09:08:09 +0100

    add posts and fiish project

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 
+import Posts from "./components/Posts"
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
           I&apos;m <span className="font-bold">Hannah</span>.
         </span>
       </p>
+      <Posts/>
     </main>
   )
 }
